13173784eb4ffd5d18c468a4b39f061b6e1fd164
laptop <shw42909@gmail.com>
2021-08-03 17:56:08 +0900

    fixing

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 max_indeed_pages = extract_indeed_pages()
 
 print(max_indeed_pages)
-extract_indeed_jobs(max_indeed_pages * 5)
+extract_indeed_jobs(max_indeed_pages)
