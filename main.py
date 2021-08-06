d001713dc7e5dc53e7824aaf4bd371aa8465608f
laptop <shw42909@gmail.com>
2021-08-06 14:12:26 +0900

    ver 0

--- main.py
+++
@@ -1,8 +0,0 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
-
-# function gets the max page number of indeed site
-
-
-max_indeed_pages = extract_indeed_pages()  # while there is next page button
-print(max_indeed_pages)
-extract_indeed_jobs(max_indeed_pages)
